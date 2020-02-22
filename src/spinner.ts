a43086c9e1b000bc3c653934dd45f448620a6480
bobby.li <bobby.lhan@gmail.com>
2020-02-22 14:52:02 +0800

    🐸  [OMNI-DOOR/TPL-UTILS]: 0.0.10

--- src/spinner.ts
+++ src/spinner.ts
@@ -4,43 +4,52 @@ import chalk from 'chalk';
 import getLogPrefix from './log_prefix';
 import { SPINNER_STATE } from './global.d';
 
+const stateMapEmoji = {
+  'start': '💤',
+  'info': '🔰',
+  'warn': '❗️',
+  'fail': '❌',
+  'succeed': '✅',
+  'stop': '❓'
+};
+
+const stateMapColor = {
+  'start': 'white' as 'white',
+  'info': 'yellowBright' as 'yellowBright',
+  'warn': 'yellow' as 'yellow',
+  'fail': 'red' as 'red',
+  'succeed': 'green' as 'green',
+  'stop': 'gray' as 'gray'
+};
+
 export class Spinner {
   private spinner: ora.Ora;
+  private _state: SPINNER_STATE;
+
   public constructor () {
     this.spinner = ora();
+    this._state = 'start';
     this.state = this.state.bind(this);
     this.text = this.text.bind(this);
     this.color = this.color.bind(this);
     this.prefix = this.prefix.bind(this);
+    this._decorateText = this._decorateText.bind(this);
   }
 
   public state (state: SPINNER_STATE, text?: string) {
+    this._state = state;
     // initial spinner
-    const stateMapEmoji = {
-      'start': '💤',
-      'info': '🔰',
-      'warn': '❗️',
-      'fail': '❌',
-      'succeed': '✅',
-      'stop': '❓'
-    };
-
-    const stateMapColor = {
-      'start': 'white' as 'white',
-      'info': 'yellowBright' as 'yellowBright',
-      'warn': 'yellow' as 'yellow',
-      'fail': 'red' as 'red',
-      'succeed': 'green' as 'green',
-      'stop': 'gray' as 'gray'
-    }
-
     text = `${getLogPrefix()} ${text}  ${stateMapEmoji[state]}  \n`;
     const color = chalk[stateMapColor[state]];
-    this.spinner[state](color(text));
+    this.spinner[state](color(this._decorateText(text)));
+  }
+
+  private _decorateText (text: string) {
+    return `${getLogPrefix()} ${text}  ${stateMapEmoji[this._state]}  \n`
   }
 
   public text (text: string) {
-    this.spinner.text = text;
+    this.spinner.text = this._decorateText(text);
   }
 
   public color (color: ora.Color) {
